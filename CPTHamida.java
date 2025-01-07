d22f1bfba483b8264d6631b9e35a3f23cc2f7108
BOB <Hamida.firoz26@ycdsbk12.ca>
2025-01-07 09:27:02 -0500

    Print hello

---
+++ CPTHamida.java
@@ -0,0 +1,12 @@
+/** start code */
+
+import arc.*; 
+
+public class CPTHamida{
+	public static void main(String[] args ){
+		Console con = new Console();
+		
+		con.print("coding this ");
+		con.println("hello");
+	}
+}
