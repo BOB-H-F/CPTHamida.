1672d9125728323f2893a5e2eecda17ea6f26cfe
BOB <Hamida.firoz26@ycdsbk12.ca>
2025-01-09 23:09:49 -0500

    started game play

--- CPTHamida.java
+++ CPTHamida.java
@@ -4,14 +4,36 @@ public class CPTHamida {
     public static void main(String[] args) {
         Console con = new Console();
 
-        // Variables
+        // Variables for All
         String StrWhere;
 
         /** Front interface */
-        con.print("Which command would you like to proceed with? ");
-        con.println("To exit press E:");
+
+        con.println("To exit press E");
+		con.println("To play game press P");
+		con.print("Which command would you like to proceed with?: ");	
         StrWhere = con.readLine();
 
-        CPTmethods.StrPage(StrWhere, con);
+        CPTmethods.StrScreen(StrWhere, con);
+        
+        /** Game play */
+        
+        // Variables
+        String strLogInName;
+      
+        
+        //play screen login: 
+       if (StrWhere.equals("P")){
+		 con.println("Login");
+		TextOutputFile Score = new TextOutputFile("Score.txt", true);
+		
+		con.print("Enter Name: ");
+		strLogInName = con.readLine();
+		Score.close();
+		
+
+	  }
+      
+       
     }
 }
---
+++ CPTmethods.java
@@ -0,0 +1,21 @@
+import arc.*;
+
+public class CPTmethods{
+	
+	
+	public static void StrScreen(String Strwhere,Console con){
+		 
+		 if (Strwhere.equals("E")) {
+            con.closeConsole(); // Close the console
+            
+		}else if (Strwhere.equals("P")) {
+		 con.clear();
+		
+		 
+		 
+
+			}
+	}
+	
+}
+
