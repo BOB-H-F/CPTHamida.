ec6a131067fa3011fc8370e7600f87e8811127c8
BOB <Hamida.firoz26@ycdsbk12.ca>
2025-01-15 01:06:47 -0500

    hi

--- CPTHamida.java
+++ CPTHamida.java
@@ -8,38 +8,44 @@ public class CPTHamida {
         String StrWhere;
 
         /** Front interface */
-
         con.println("To exit press E");
-		con.println("To play game press P");
-		con.print("Which command would you like to proceed with?: ");	
+        con.println("To play game press P");
+        con.println("To see score press S");
+        con.print("Which command would you like to proceed with?: ");    
         StrWhere = con.readLine();
 
         CPTmethods.StrScreen(StrWhere, con);
-        
+
         /** Game play */
         
         // Variables
         String strLogInName;
-		double dblMark; 
-		double dbBlcasicMath;
-		double dblCount;
-        //play screen login: 
-       if (StrWhere.equals("P")){
-		 con.println("Login");
-		TextOutputFile Score = new TextOutputFile("Score.txt", true);
-		
-		con.print("Enter Name: ");
-		strLogInName = con.readLine();
-		
-		for dblCount
-		con.println("");
-		
-		Score.close();
-		
-		
+        double dblMark; 
+        double dbBlcasicMath;
+        double dblCount;
 
-	  }
-      
-       
+        // play screen login: 
+        if (StrWhere.equals("P")) {
+            con.println("Login");
+            TextOutputFile Score = new TextOutputFile("Login.txt", true);  // Open the file in append mode
+
+            con.print("Enter Name: ");
+            strLogInName = con.readLine();
+
+            // Write the name into the file
+            Score.println(strLogInName);  
+
+            // Ask the user for their score
+            con.print("Enter your score: ");
+            dblMark = con.readDouble();
+
+            // Write the score into the file
+            Score.println(dblMark);  
+
+
+            Score.close();  
+
+            
+        }
     }
 }
